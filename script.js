25ba80d5328706b05d79f66ca38afbda77283f64
LuisOrihuela08 <luis.orihuela@upeu.edu.pe>
2025-01-24 17:43:32 -0500

    añadiendo iconos de lenguajes en la seccion de skill

--- script.js
+++ script.js
@@ -20,16 +20,16 @@ function efectoHabilidades() {
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if (distancia_skills >= 300) {
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("java");
-        habilidades[3].classList.add("python");
-        habilidades[4].classList.add("sql");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("creatividad");
-        habilidades[7].classList.add("trabajo");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        //habilidades[0].classList.add("javascript");
+       // habilidades[1].classList.add("htmlcss");
+       // habilidades[2].classList.add("java");
+        //habilidades[3].classList.add("python");
+       // habilidades[4].classList.add("sql");
+        habilidades[0].classList.add("comunicacion");
+        habilidades[1].classList.add("creatividad");
+        habilidades[2].classList.add("trabajo");
+        habilidades[3].classList.add("dedicacion");
+        habilidades[4].classList.add("proyect");
     }
 }
 
